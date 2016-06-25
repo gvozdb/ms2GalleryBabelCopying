08b3dc60020051a2b43993c129ab6f58e15d245b
gvozdb <i@owl.kz>
2016-06-25 19:41:13 +0500

    1.0.1-pl2

--- _build/build.config.php
+++ _build/build.config.php
@@ -5,7 +5,7 @@ define('PKG_NAME', 'ms2GalleryBabelCopying');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
 define('PKG_VERSION', '1.0.1');
-define('PKG_RELEASE', 'pl');
+define('PKG_RELEASE', 'pl2');
 define('PKG_AUTO_INSTALL', true);
 define('PKG_NAMESPACE_PATH', '{core_path}components/' . PKG_NAME_LOWER . '/');
 
